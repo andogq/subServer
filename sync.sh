30cc7ef90a89b88a4c8c999e8e9b4824e3663e27
Tom Anderson <portlester.tom@gmail.com>
2018-04-25 18:15:13 +1000

    Fixed up sync script

--- sync.sh
+++ sync.sh
@@ -7,8 +7,9 @@ if [ -z "$DEST" ]; then exit 1; fi
 inotifywait -r -m -e close_write --format '%w%f' . |\
 while read file
 do
-        echo $file
-	rsync -azvq $file ${DEST}/$file
+    file=${file:2}
+    echo "rsync -azvq ./$file ${DEST}"
+	rsync -azvq ./$file ${DEST}
 	echo -n 'Completed at '
 	date
 done
